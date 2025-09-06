081bee3e3bd9a8f4d2eb1fe3a9b21ab1bae2149b
LepharamRamchiary <lepharamchiary@gmail.com>
2025-09-06 14:10:30 +0530

    added login funconility means added api for login

--- laravel-project/app/Http/Controllers/Api/AuthController.php
+++ laravel-project/app/Http/Controllers/Api/AuthController.php
@@ -35,7 +35,7 @@ class AuthController extends Controller
         if (!Auth::attempt($credentials)) {
             return response([
                 'message' => 'The provided credentials are incorrect'
-            ]);
+            ], 422);
         }
 
         $user = Auth::user();
